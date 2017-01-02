017a6b5203967eaf038a22b69102d63245196720
Alberto Alvarez Aldea <alberto.alvarez.aldea@hotmail.com>
2017-01-02 13:32:30 +0100

    Changes functions order

--- src/irq_latency.c
+++ src/irq_latency.c
@@ -36,11 +36,14 @@ void latency_exit(void);
 
 static int latencty_open(struct inode *inode, struct file *fp);
 
-static int latency_read(struct file *fp);
+static int latency_close(struct inode *inode, struct file *fp);
 
 static int latency_ioctl(struct file *fp, int period);
 
-static int latency_close(struct inode *inode, struct file *fp);
+static int latency_read(struct file *fp);
+
+
+
 
 MODULE_AUTHOR("ALBERTO ALVAREZ (alberto.alvarez.aldea@gmail.com)");
 MODULE_DESCRIPTION("Measure interrupt latency");
--- src/irq_latency.h
+++ src/irq_latency.h
@@ -16,9 +16,9 @@
 int latency_init(void);
 void latency_exit(void);
 static int latencty_open(struct inode *inode, struct file *fp);
-static int latency_read(struct file *fp);
-static int latency_ioctl(struct file *fp, int period);
 static int latency_close(struct inode *inode, struct file *fp);
+static int latency_ioctl(struct file *fp, int period);
+static int latency_read(struct file *fp);
 
 /* Variables */
 struct cdev *mcdev;
