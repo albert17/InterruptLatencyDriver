7af7a667b49de270c691c25ca5658289d502de44
Alberto Alvarez Aldea <alberto.alvarez.aldea@hotmail.com>
2017-01-02 13:31:24 +0100

    Adds init function

--- src/irq_latency.c
+++ src/irq_latency.c
@@ -1,7 +1,36 @@
 #include "irq_latency.h"
 
 
-int latency_init(void);
+int latency_init(void) {
+
+    // obtain major number dinamically
+    ret = alloc_chrdev_region(&dev_num,0,1,DEVICENAME);
+    if(ret < 0) {
+        printk(KERN_ALERT " irq_latency : failed to allocate major number\n");
+        return ret;
+    }
+    else {
+        printk(KERN_INFO " irq_latency : major number allocated succesful\n");
+    }
+    major_number = MAJOR(dev_num);
+    printk(KERN_INFO "irq_latency : major number of device is %d\n",major_number);
+
+     // Create, allocate and initialize cdev structure
+    lcdev = cdev_alloc();
+    lcdev->ops = &fops;
+    lcdev->owner = THIS_MODULE;
+
+    // Add cdev structure to the kernel
+    ret = cdev_add(lcdev,dev_num,1);
+    if(ret < 0) {
+        printk(KERN_ALERT "irq_latency : device adding to the kerknel failed\n");
+        return ret;
+    }
+    else {
+        printk(KERN_INFO "irq_latency : device additin to the kernel succesful\n");
+    }
+    return 0;
+}
 
 void latency_exit(void);
 
