8df683d59e470423e29c5dbe1d92e86f55658a9d
Alberto Alvarez Aldea <alberto.alvarez.aldea@hotmail.com>
2017-02-01 19:28:07 +0100

    Compiles driver and example

--- src/driver/latency.h
+++ src/driver/latency.h
@@ -1,6 +1,9 @@
 #ifndef LATENCY_H
 #define LATENCY_H
 
+#include <linux/ioctl.h>
+#include <linux/types.h>
+
 /* Defines */
 #define D_NAME "latency"
 #define LATENCY_IOC_MAGIC '$'
@@ -13,13 +16,13 @@ enum io {
 };
 
 struct latency_buffer{
-     u16 irq_pin;
-     u16 gpio_pin;
+     uint16_t irq_pin;
+     uint16_t gpio_pin;
      int period;
 };
 
 #define ISET _IOW(LATENCY_IOC_MAGIC, SET, struct latency_buffer)
-#define ION _IO(LATENCY_IOC_MAGIC, ON, struct latency_buffer)
-#define IOFF _IO(LATENCY_IOC_MAGIC, OFF, struct latency_buffer)
+#define ION _IO(LATENCY_IOC_MAGIC, ON)
+#define IOFF _IO(LATENCY_IOC_MAGIC, OFF)
 
 #endif
