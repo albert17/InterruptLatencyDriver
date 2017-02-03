6f2c3be49d4646d060082a2886d747b8a8e6bcea
Alberto Alvarez Aldea <alberto.alvarez.aldea@hotmail.com>
2017-02-03 01:22:37 +0100

    Comments and improves irq.h

--- src/driver/irq.h
+++ src/driver/irq.h
@@ -2,12 +2,17 @@
 #define IRQ_H
 
 #include <linux/interrupt.h>
+
 #include "latency.h"
 
+// ARM control base value
 #define AM33XX_CONTROL_BASE 0x44e10000
+// Pin as output value
 #define OUTPUT 0x7 | (2 << 3)
+// Pin as input value
 #define INPUT 0x7 | (2 << 3) | (1 << 5)
 
+// latency_dev structure
 struct latency_dev {
    struct timespec gpio_time, irq_time;
    u16 irq;
@@ -20,10 +25,39 @@ struct latency_dev {
    struct device dev;
 };
 
-int setup_pinmux(struct latency_dev *latency_devp);
-int configure_gpio_irq(struct latency_dev *latency_devp);
-void release_gpio_irq(struct latency_dev *latency_devp);
+/**
+ * Configures the pins incated in ldev structure
+ * @param ldev structure with all the contents
+ * @return 0 in success, negative in fail.
+ */
+int setup_pinmux(struct latency_dev *ldev);
+
+/**
+ * Configure the gpio and the irq.
+ * @param ldev structure with all the contents
+ * @return 0 in success, negative in fail.
+ */
+int configure_gpio_irq(struct latency_dev *ldev);
+
+/**
+ * Release the gpio and the irq.
+ * @param ldev structure with all the contents
+ */
+void release_gpio_irq(struct latency_dev *ldev);
+
+/**
+ * Handles the irq setting a flag in the structure.
+ * @param irq irq interrupt number
+ * @param dev_id ldev structure with all the contents
+ * @return irq handled code
+ */
 irqreturn_t irq_handler(int irq, void* dev_id);
+
+/**
+ * Handles timer interrrupt, calculating the times and 
+ * reconfiguring the timer.
+ * @param
+ */
 void timer_handler(unsigned long ptr);
 
 #endif /* IRQ_H */
